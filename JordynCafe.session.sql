80e55e523afc771d86224294946608d47f4b6dfc
Jordyn Merchant <jo.merchant96@gmail.com>
2023-02-28 14:05:43 -0700

    f

--- JordynCafe.session.sql
+++ JordynCafe.session.sql
@@ -1,8 +1,9 @@
 CREATE TABLE CafeItems (
     men_food VARCHAR(30) NOT NULL,
-    calories VARCHAR(6) NOT NULL,
+    calories VARCHAR(6) NOT NULL PRIMARY KEY,
     ItemDesc VARCHAR(150) NOT NULL,
     mealTime VARCHAR(25) NOT NULL,
-    PRIMARY KEY(calories),
-    FOREIGN KEY(men_food)
-)
+    FOREIGN KEY(men_food) REFERENCES CafeMenu(men_food)
+);
+
+
