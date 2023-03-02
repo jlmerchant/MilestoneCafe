67ac0cb87b92f7c335d33f3b83f3b12769911424
Jordyn Merchant <jo.merchant96@gmail.com>
2023-03-01 20:57:46 -0700

    fin

--- JordynCafe.session.sql
+++ JordynCafe.session.sql
@@ -1,8 +1,3 @@
 CREATE TABLE Classify (
-    classType SET('classic','lite fare') NOT NULL,
-    calories VARCHAR(6) NOT NULL,
-    men_food VARCHAR(30) NOT NULL,
-    PRIMARY KEY (classType),
-    FOREIGN KEY (calories) REFERENCES CafeItems(calories),
-    FOREIGN KEY (men_food) REFERENCES CafeMenu(men_food)
-);
+    classType 
+)
